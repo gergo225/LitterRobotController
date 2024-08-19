76deaad4e6406e891672d7eb3ada3e920eebbba7
Fazekas Gergo <fazekasgergo225@gmail.com>
2024-08-19 22:08:59 +0300

    Change naming and characteristic to accept a value written from the client.
    Currently only prints the value from the client

--- LitterRobotControllerServer/src/main.cpp
+++ LitterRobotControllerServer/src/main.cpp
@@ -10,34 +10,37 @@
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
-#include <BLE2902.h>
 
 //BLE server name
 #define bleServerName "Litter Robot Motor"
-
-float hum;
-
-// Timer variables
-unsigned long lastTime = 0;
-unsigned long timerDelay = 30000;
 
 bool deviceConnected = false;
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 #define SERVICE_UUID "91bad492-b950-4226-aa2b-4ede9fa42f59"
+#define CHARACTERISTIC_UUID "ca73b3ba-39f6-4ab3-91ae-186dc9577d99"
 
 // Humidity Characteristic and Descriptor
-BLECharacteristic bmeHumidityCharacteristics("ca73b3ba-39f6-4ab3-91ae-186dc9577d99", BLECharacteristic::PROPERTY_NOTIFY);
-BLEDescriptor bmeHumidityDescriptor(BLEUUID((uint16_t)0x2903));
+BLECharacteristic motorCharacteristics(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_WRITE);
+BLEDescriptor motorDescriptor(BLEUUID((uint16_t)0x2902));
 
 //Setup callbacks onConnect and onDisconnect
 class MyServerCallbacks: public BLEServerCallbacks {
   void onConnect(BLEServer* pServer) {
-    deviceConnected = true;
+    Serial.println("Client connected");
   };
   void onDisconnect(BLEServer* pServer) {
-    deviceConnected = false;
+    Serial.println("Client disconnected");
+  }
+};
+
+class MotorCharacteristicsCallbacks: public BLECharacteristicCallbacks {
+	void onWrite(BLECharacteristic* pCharacteristic, esp_ble_gatts_cb_param_t* param) {
+    const String value = pCharacteristic->getValue().c_str();
+
+    Serial.print("Got value from client: ");
+    Serial.println(value);
   }
 };
 
@@ -53,40 +56,23 @@ void setup() {
   pServer->setCallbacks(new MyServerCallbacks());
 
   // Create the BLE Service
-  BLEService *bmeService = pServer->createService(SERVICE_UUID);
+  BLEService *motorService = pServer->createService(SERVICE_UUID);
 
   // Humidity
-  bmeService->addCharacteristic(&bmeHumidityCharacteristics);
-  bmeHumidityDescriptor.setValue("BME humidity");
-  bmeHumidityCharacteristics.addDescriptor(new BLE2902());
+  motorService->addCharacteristic(&motorCharacteristics);
+  motorDescriptor.setValue("Motor Control");
+  motorCharacteristics.addDescriptor(&motorDescriptor);
+  motorCharacteristics.setCallbacks(new MotorCharacteristicsCallbacks());
   
   // Start the service
-  bmeService->start();
+  motorService->start();
 
   // Start advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pServer->getAdvertising()->start();
-  Serial.println("Waiting a client connection to notify...");
+  Serial.println("Waiting a client connection to connect...");
 }
 
 void loop() {
-  if (deviceConnected) {
-    if ((millis() - lastTime) > timerDelay) {
-      // Read humidity
-      hum = 702;  // this is just a dummy value
-  
-      //Notify humidity reading from BME
-      static char humidityTemp[6];
-      dtostrf(hum, 6, 2, humidityTemp);
-      //Set humidity Characteristic value and notify connected client
-      bmeHumidityCharacteristics.setValue(humidityTemp);
-      bmeHumidityCharacteristics.notify();   
-      Serial.print(" - Humidity: ");
-      Serial.print(hum);
-      Serial.println(" %");
-      
-      lastTime = millis();
-    }
-  }
 }
