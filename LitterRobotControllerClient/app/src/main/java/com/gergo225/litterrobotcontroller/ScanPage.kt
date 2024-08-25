fcdf7b400932d76541bf6e05dc1b17cb1de8db06
Fazekas Gergo <fazekasgergo225@gmail.com>
2024-08-25 18:42:56 +0300

    Connect client to 'LitterRobot Controller' when it finds it among scanned devices

--- LitterRobotControllerClient/app/src/main/java/com/gergo225/litterrobotcontroller/ScanPage.kt
+++ LitterRobotControllerClient/app/src/main/java/com/gergo225/litterrobotcontroller/ScanPage.kt
@@ -3,6 +3,8 @@ package com.gergo225.litterrobotcontroller
 import android.bluetooth.BluetoothManager
 import android.content.Context
 import androidx.annotation.RequiresPermission
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
@@ -13,6 +15,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
@@ -20,7 +23,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 fun ScanPage(viewModel: ScanViewModel = viewModel()) {
     val context = LocalContext.current
 
-    val foundDevices by viewModel.foundDevices.collectAsState()
+    val scanState by viewModel.scanState.collectAsState()
 
     LaunchedEffect(viewModel) {
         val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as? BluetoothManager
@@ -28,15 +31,37 @@ fun ScanPage(viewModel: ScanViewModel = viewModel()) {
     }
 
 
-    if (foundDevices.isEmpty()) {
-        CircularProgressIndicator()
-    } else {
-        LazyColumn(
-            horizontalAlignment = Alignment.Start
-        ) {
-            items(foundDevices) {
-                Text(it.name ?: it.address)
-            }
-        }
+    when (scanState) {
+        ScanState.SCANNING -> ScanInProgressScreen()
+        ScanState.CONNECTING -> ConnectingToDeviceScreen()
+        ScanState.CONNECTED -> ConnectedToDeviceScreen()
     }
+}
+
+@Composable
+private fun ScanInProgressScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text("Searching for device")
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+private fun ConnectingToDeviceScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text("Found device")
+        Text("Connecting to device...")
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+private fun ConnectedToDeviceScreen() {
+    Text("Connected to 'LitterRobot Motor' device")
 }
--- LitterRobotControllerClient/app/src/main/java/com/gergo225/litterrobotcontroller/ScanViewModel.kt
+++ LitterRobotControllerClient/app/src/main/java/com/gergo225/litterrobotcontroller/ScanViewModel.kt
@@ -1,39 +1,66 @@
 package com.gergo225.litterrobotcontroller
 
+import android.app.Application
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.util.Log
 import androidx.annotation.RequiresPermission
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 
-class ScanViewModel : ViewModel() {
+enum class ScanState {
+    SCANNING,
+    CONNECTING,
+    CONNECTED
+}
+
+class ScanViewModel(private val application: Application) : AndroidViewModel(application) {
     private var scanner: BluetoothLeScanner? = null
+    private var bluetoothDevice: BluetoothDevice? = null
 
-    private val _foundDevices = MutableStateFlow<List<BluetoothDevice>>(emptyList())
-    val foundDevices = _foundDevices.asStateFlow()
+    private val _scanState = MutableStateFlow(ScanState.SCANNING)
+    val scanState = _scanState.asStateFlow()
+
+    companion object {
+        private const val MOTOR_CONTROLLER_DEVICE_NAME = "Litter Robot Motor"
+    }
 
     private val scanCallback = object : ScanCallback() {
+        @RequiresPermission(allOf = [BluetoothPermissions.BLUETOOTH_CONNECT, BluetoothPermissions.BLUETOOTH_SCAN])
         override fun onScanResult(callbackType: Int, result: ScanResult?) {
             super.onScanResult(callbackType, result)
 
             result ?: return
             val device = result.device
-            if (!foundDevices.value.contains(device)) {
-                _foundDevices.update { it + device }
+            if (device.name == MOTOR_CONTROLLER_DEVICE_NAME) {
+                foundBluetoothDevice(device)
             }
-
-            // TODO: see if found device with needed UUID
         }
 
         override fun onScanFailed(errorCode: Int) {
             super.onScanFailed(errorCode)
             Log.w("ScanViewModel (scan callback)", "Scan failed")
+        }
+    }
+
+    private val connectCallback = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
+            super.onConnectionStateChange(gatt, status, newState)
+
+            if (status != BluetoothGatt.GATT_SUCCESS) {
+                // TODO: handle connection error (display on UI)
+                Log.w("ScanViewModel (connection callback)", "Failed connecting to device")
+            }
+
+            if (newState == BluetoothGatt.STATE_CONNECTED) {
+                connectedToDevice()
+            }
         }
     }
 
@@ -45,6 +72,24 @@ class ScanViewModel : ViewModel() {
 
     @RequiresPermission(BluetoothPermissions.BLUETOOTH_SCAN)
     private fun startScanning() {
+        _scanState.value = ScanState.SCANNING
         scanner?.startScan(scanCallback)
+    }
+
+    @RequiresPermission(allOf = [BluetoothPermissions.BLUETOOTH_SCAN, BluetoothPermissions.BLUETOOTH_CONNECT])
+    private fun foundBluetoothDevice(device: BluetoothDevice) {
+        scanner?.stopScan(scanCallback)
+        bluetoothDevice = device
+        connectToDevice()
+    }
+
+    @RequiresPermission(BluetoothPermissions.BLUETOOTH_CONNECT)
+    private fun connectToDevice() {
+        _scanState.value = ScanState.CONNECTING
+        bluetoothDevice?.connectGatt(application, false, connectCallback)
+    }
+
+    private fun connectedToDevice() {
+        _scanState.value = ScanState.CONNECTED
     }
 }
